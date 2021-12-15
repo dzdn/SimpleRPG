0158c8f83f6512bfea653840ed1a331e24af04bc
dzdn <jdustinwilliams@gmail.com>
2021-12-14 20:19:16 -0500

    Complete part 8.

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -4,6 +4,9 @@ extends KinematicBody2D
 # Player movement speed
 export var speed = 75
 
+var last_direction = Vector2(0, 1)
+var attack_playing = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,4 +24,52 @@ func _physics_process(delta):
 	
 	# Apply movement
 	var movement = speed * direction * delta
+	if attack_playing:
+		movement = 0.3 * movement
 	move_and_collide(movement)
+	
+	# Animate player based on direction
+	if not attack_playing:
+		animates_player(direction)
+
+func animates_player(direction: Vector2):
+	if direction != Vector2.ZERO:
+		# update last_direction
+		last_direction = 0.5 * last_direction + 0.5  * direction
+		
+		# choose walk animation based on movement direction
+		var animation = get_animation_direction(last_direction) + "_walk"
+		
+		# play the walk animation
+		$Sprite.frames.set_animation_speed(animation, 2 + 8 * direction.length())
+		$Sprite.play(animation)
+	else:
+		# choose idle animation based on last movement direction and play it
+		var animation = get_animation_direction(last_direction) + "_idle"
+		$Sprite.play(animation)
+
+func get_animation_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y >= 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <= -0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
+
+func _input(event):
+	if event.is_action_pressed("attack"):
+		attack_playing = true
+		var animation = get_animation_direction(last_direction) + "_attack"
+		$Sprite.play(animation)
+	elif event.is_action_pressed("fireball"):
+		attack_playing = true
+		var animation = get_animation_direction(last_direction) + "_fireball"
+		$Sprite.play(animation)
+
+
+func _on_Sprite_animation_finished():
+	attack_playing = false
